45ec9ac1943d4f4ec28a3c8c157216e86bc39953
Jan Dusek <j@koiru.com>
2021-01-26 00:18:00 +0800

    Added macro for mission control Cmd+Shift+Up

--- keyboards/handwired/dactyl_manuform/5x6/keymaps/jandusek/keymap.c
+++ keyboards/handwired/dactyl_manuform/5x6/keymaps/jandusek/keymap.c
@@ -6,6 +6,10 @@
 #define _RAISE 2
 #define _ADJUST 16
 
+enum custom_keycodes {
+    M_MIS_CON = SAFE_RANGE,
+};
+
 #define RAISE MO(_RAISE)
 #define LOWER MO(_LOWER)
 #define ADJUST MO(_ADJUST)
@@ -26,22 +30,22 @@ uint32_t layer_state_set_user(uint32_t state) {
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
   [_QWERTY] = LAYOUT_5x6(
-     KC_GRAVE , KC_1  , KC_2  , KC_3  , KC_4  , KC_5  ,                      KC_6  , KC_7  , KC_8  , KC_9  , KC_0  ,KC_BSPC,
+     KC_GRAVE , KC_1  , KC_2  , KC_3  , KC_4  , KC_5  ,                      KC_6  , KC_7  , KC_8  , KC_9  , KC_0  ,KC_MINS,
      KC_TAB ,   KC_Q  , KC_W  , KC_E  , KC_R  , KC_T  ,                      KC_Y  , KC_U  , KC_I  , KC_O  , KC_P , KC_BSLASH,
      KC_ESC,    KC_A  , KC_S  , KC_D  , KC_F  , KC_G  ,                      KC_H  , KC_J  , KC_K  , KC_L  ,KC_SCLN, KC_QUOT,
-     KC_LSFT,   KC_Z  , KC_X  , KC_C  , KC_V  , KC_B  ,                      KC_N  , KC_M  ,KC_COMM,KC_DOT ,KC_SLSH, KC_MINS,
+     KC_LSFT,   KC_Z  , KC_X  , KC_C  , KC_V  , KC_B  ,                      KC_N  , KC_M  ,KC_COMM,KC_DOT ,KC_SLSH, KC_BSPC,
                       KC_LALT, KC_LCTL,                                                     KC_EQUAL, _______,
                                       RAISE, KC_LGUI,                        KC_SPC, LOWER,
                                       KC_ENT, KC_HOME,                       KC_END,  KC_DEL,
-                                      KC_BSPC, _______,                      _______, KC_LALT
+                                      KC_BSPC, _______,                      M_MIS_CON, KC_LALT
   ),
 
   [_LOWER] = LAYOUT_5x6(
 
-     KC_TILD,KC_EXLM, KC_AT ,KC_HASH,KC_DLR ,KC_PERC,                        KC_CIRC,KC_AMPR,KC_ASTR,KC_LPRN,KC_RPRN,KC_DEL,
+     KC_TILD,KC_EXLM, KC_AT ,KC_HASH,KC_DLR ,KC_PERC,                        KC_CIRC,KC_AMPR,KC_ASTR,KC_LPRN,KC_RPRN,KC_UNDS,
      _______,_______,_______,_______,_______,KC_LBRC,                        KC_RBRC, KC_P7 , KC_P8 , KC_P9 ,_______,KC_PLUS,
      _______,KC_HOME,KC_PGUP,KC_PGDN,KC_END ,KC_LPRN,                        KC_RPRN, KC_P4 , KC_P5 , KC_P6 ,KC_MINS,KC_PIPE,
-     _______,_______,_______,_______,_______,KC_LCBR,                        KC_RCBR, KC_P1 , KC_P2 , KC_P3 ,KC_EQL ,KC_UNDS,
+     _______,_______,_______,_______,_______,KC_LCBR,                        KC_RCBR, KC_P1 , KC_P2 , KC_P3 ,KC_EQL ,KC_DEL,
                      _______,KC_PSCR,                                                         KC_P0, _______, 
                                      _______,_______,                        _______,_______,
                                      _______,_______,                        _______,_______,
@@ -139,8 +143,10 @@ void rgblight_set_hsv_and_mode(uint8_t hue, uint8_t sat, uint8_t val, uint8_t mo
     wait_us(175);  // Add a slight delay between color and mode to ensure it's processed correctly
     rgblight_mode_noeeprom(mode);
 }
+#endif
 
 bool process_record_user(uint16_t keycode, keyrecord_t *record) {
+#ifdef RGBLIGHT_ENABLE
     if (record->event.pressed) {
         if (rgblight_is_enabled() == false) {
             rgblight_enable_noeeprom();
@@ -148,9 +154,19 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
         idle_timer = timer_read();
         halfmin_counter = 0;
     }
+#endif
+    switch (keycode) {
+        case M_MIS_CON:
+            if (record->event.pressed) {
+                // when keycode QMKBEST is pressed
+                SEND_STRING(SS_LGUI(SS_LSFT(SS_TAP(X_UP))));
+            } else {
+                // when keycode QMKBEST is released
+            }
+            break;
+    }
     return true;
 }
-#endif
 
 void matrix_scan_user(void) {
     if (is_gui_tab_active) {
