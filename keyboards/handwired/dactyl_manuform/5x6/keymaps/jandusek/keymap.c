481891d5a03d58d5735768d5877bfb7a311c3068
Jan Dusek <jacobs@Yaeger.localdomain>
2021-01-25 14:00:43 +0800

    Dactyl updates

--- keyboards/handwired/dactyl_manuform/5x6/keymaps/jandusek/keymap.c
+++ keyboards/handwired/dactyl_manuform/5x6/keymaps/jandusek/keymap.c
@@ -26,14 +26,14 @@ uint32_t layer_state_set_user(uint32_t state) {
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
   [_QWERTY] = LAYOUT_5x6(
-     KC_TAB , KC_1  , KC_2  , KC_3  , KC_4  , KC_5  ,                         KC_6  , KC_7  , KC_8  , KC_9  , KC_0  ,KC_BSPC,
-     KC_ESC , KC_Q  , KC_W  , KC_E  , KC_R  , KC_T  ,                         KC_Y  , KC_U  , KC_I  , KC_O  , KC_P  ,KC_MINS,
-     KC_LSFT, KC_A  , KC_S  , KC_D  , KC_F  , KC_G  ,                         KC_H  , KC_J  , KC_K  , KC_L  ,KC_SCLN,KC_QUOT,
-     KC_LCTL, KC_Z  , KC_X  , KC_C  , KC_V  , KC_B  ,                         KC_N  , KC_M  ,KC_COMM,KC_DOT ,KC_SLSH,KC_BSLASH,
-                      KC_LBRC,KC_RBRC,                                                       KC_PLUS, KC_EQL,
-                                      RAISE, KC_ENT,                          KC_SPC, LOWER,
-                                      KC_LGUI, KC_HOME,                       KC_END,  KC_DEL,
-                                      KC_BSPC, KC_GRV,                        _______, KC_LALT
+     KC_GRAVE , KC_1  , KC_2  , KC_3  , KC_4  , KC_5  ,                      KC_6  , KC_7  , KC_8  , KC_9  , KC_0  ,KC_BSPC,
+     KC_TAB ,   KC_Q  , KC_W  , KC_E  , KC_R  , KC_T  ,                      KC_Y  , KC_U  , KC_I  , KC_O  , KC_P , KC_BSLASH,
+     KC_ESC,    KC_A  , KC_S  , KC_D  , KC_F  , KC_G  ,                      KC_H  , KC_J  , KC_K  , KC_L  ,KC_SCLN, KC_QUOT,
+     KC_LSFT,   KC_Z  , KC_X  , KC_C  , KC_V  , KC_B  ,                      KC_N  , KC_M  ,KC_COMM,KC_DOT ,KC_SLSH, KC_MINS,
+                      KC_LALT, KC_LCTL,                                                     KC_EQUAL, _______,
+                                      RAISE, KC_LGUI,                        KC_SPC, LOWER,
+                                      KC_ENT, KC_HOME,                       KC_END,  KC_DEL,
+                                      KC_BSPC, _______,                      _______, KC_LALT
   ),
 
   [_LOWER] = LAYOUT_5x6(
@@ -41,8 +41,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
      KC_TILD,KC_EXLM, KC_AT ,KC_HASH,KC_DLR ,KC_PERC,                        KC_CIRC,KC_AMPR,KC_ASTR,KC_LPRN,KC_RPRN,KC_DEL,
      _______,_______,_______,_______,_______,KC_LBRC,                        KC_RBRC, KC_P7 , KC_P8 , KC_P9 ,_______,KC_PLUS,
      _______,KC_HOME,KC_PGUP,KC_PGDN,KC_END ,KC_LPRN,                        KC_RPRN, KC_P4 , KC_P5 , KC_P6 ,KC_MINS,KC_PIPE,
-     _______,_______,_______,_______,_______,_______,                        _______, KC_P1 , KC_P2 , KC_P3 ,KC_EQL ,KC_UNDS,
-                     _______,KC_PSCR,                                                        _______, KC_P0,
+     _______,_______,_______,_______,_______,KC_LCBR,                        KC_RCBR, KC_P1 , KC_P2 , KC_P3 ,KC_EQL ,KC_UNDS,
+                     _______,KC_PSCR,                                                         KC_P0, _______, 
                                      _______,_______,                        _______,_______,
                                      _______,_______,                        _______,_______,
                                      _______,_______,                        _______,_______
@@ -50,25 +50,25 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   ),
 
   [_RAISE] = LAYOUT_5x6(
-       KC_F12 , KC_F1 , KC_F2 , KC_F3 , KC_F4 , KC_F5 ,                        KC_F6  , KC_F7 , KC_F8 , KC_F9 ,KC_F10 ,KC_F11 ,
-       _______,_______,_______,_______,_______,KC_LBRC,                        KC_RBRC,_______,KC_NLCK,KC_INS ,KC_SLCK,KC_MUTE,
-       _______,KC_LEFT,KC_UP  ,KC_DOWN,KC_RGHT,KC_LPRN,                        KC_RPRN,KC_MPRV,KC_MPLY,KC_MNXT,_______,KC_VOLU,
-       _______,_______,_______,_______,_______,_______,                        _______,_______,_______,_______,_______,KC_VOLD,
-                       _______,_______,                                                        KC_EQL ,_______,
-                                       _______,_______,                        _______,_______,
-                                       _______,_______,                        _______,_______,
-                                       _______,_______,                        _______,_______
+       KC_F12 , KC_F1 , KC_F2 , KC_F3 , KC_F4 , KC_F5 ,                      KC_F6  , KC_F7 , KC_F8 , KC_F9 ,KC_F10 ,KC_F11 ,
+       _______,_______,_______,_______,_______,KC_LBRC,                      KC_RBRC,_______,KC_NLCK,KC_INS ,KC_SLCK,KC_MUTE,
+       _______,KC_LEFT,KC_UP  ,KC_DOWN,KC_RGHT,KC_LPRN,                      KC_RPRN,KC_MPRV,KC_MPLY,KC_MNXT,_______,KC_VOLU,
+       _______,_______,_______,_______,_______,KC_LCBR,                      KC_RCBR,_______,_______,_______,_______,KC_VOLD,
+                       _______,_______,                                                      KC_EQL ,_______,
+                                       _______,_______,                      _______,_______,
+                                       _______,_______,                      _______,_______,
+                                       _______,_______,                      _______,_______
   ),
 
   [_ADJUST] = LAYOUT_5x6(
-       _______,_______,_______,_______,_______,_______,                        _______,_______,_______,_______,_______,_______,
-       RGB_TOG,RGB_HUI,RGB_SAI,RGB_VAI,_______,_______,                        _______,_______,_______,_______,_______,_______,
-       RGB_MOD,RGB_HUD,RGB_SAD,RGB_VAD,_______,_______,                        _______,_______,_______,_______,_______,_______,
-       RGB_M_P,RGB_M_B,RGB_M_R,_______,_______,_______,                        _______,_______,_______,_______,_______,_______,
-                       _______,_______,                                                        _______,_______,
-                                       _______,_______,                        _______,_______,
-                                       _______,_______,                        _______,_______,
-                                       _______,_______,                        _______,_______
+       _______,_______,_______,_______,_______,_______,                      _______,_______,_______,_______,_______,_______,
+       RGB_TOG,RGB_HUI,RGB_SAI,RGB_VAI,_______,_______,                      _______,_______,_______,_______,_______,_______,
+       RGB_MOD,RGB_HUD,RGB_SAD,RGB_VAD,_______,_______,                      _______,_______,_______,_______,_______,_______,
+       RGB_M_P,RGB_M_B,RGB_M_R,_______,_______,_______,                      _______,_______,_______,_______,_______,_______,
+                       _______,_______,                                                      _______,_______,
+                                       _______,_______,                      _______,_______,
+                                       _______,_______,                      _______,_______,
+                                       _______,_______,                      _______,_______
   ),   
 };
 
@@ -139,8 +139,18 @@ void rgblight_set_hsv_and_mode(uint8_t hue, uint8_t sat, uint8_t val, uint8_t mo
     wait_us(175);  // Add a slight delay between color and mode to ensure it's processed correctly
     rgblight_mode_noeeprom(mode);
 }
-#endif
 
+bool process_record_user(uint16_t keycode, keyrecord_t *record) {
+    if (record->event.pressed) {
+        if (rgblight_is_enabled() == false) {
+            rgblight_enable_noeeprom();
+        }
+        idle_timer = timer_read();
+        halfmin_counter = 0;
+    }
+    return true;
+}
+#endif
 
 void matrix_scan_user(void) {
     if (is_gui_tab_active) {
